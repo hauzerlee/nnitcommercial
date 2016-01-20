c82f389e5a16a2d03efd5336b6124739edb93186
jaly shen <sjl.jaly@gmail.com>
2016-01-20 21:24:36 +0800

    add new methods

--- src/com/nnit/solution/local/Constant.py
+++ src/com/nnit/solution/local/Constant.py
@@ -10,26 +10,6 @@
 
 import sys
 
-
-class Constant(object):
-
-    def __setattr__(self, key, value):
-        if self.__dict__.has_key(key):
-            raise ConstantError
-        else:
-            self.__dict__[key] = value
-
-    def __getattr__(self, key):
-        if self.__dict__.has_key(key):
-            return self.key
-        else:
-            return None
-
-
-class ConstantError(TypeError):
-        def __init__(self):
-            pass
-
 # 排序方式
 SEARCHING_DIRECTION_ASC = 'asc'
 SEARCHING_DIRECTION_DESC = 'desc'
@@ -50,5 +30,3 @@ INTEGRAL = 'Integral:'
 HOT_GROUPONS = 'Hot_Groupons'
 DISCOUNTS_SORT = 'Discounts:discountSort'
 DISCOUNT = 'Discount:'
-
-sys.modules[__name__]=Constant()
--- src/com/nnit/solution/redisdao/shop_redis_dao.py
+++ src/com/nnit/solution/redisdao/shop_redis_dao.py
@@ -8,6 +8,7 @@
 和商户相关的操作，都定义在这个dao中
 """
 
+import sys
 import redis
 from com.nnit.solution.local import Constant
 from com.nnit.solution.local.util import utils
@@ -17,7 +18,11 @@ class ShopRedisDAO(object):
 
     def __init__(self):
         # get the redis connection
-        self.redis = utils.RedisConnection.get_redis_connection()
+        self.redis = utils.RedisConnection.get_redis_connection('localhost',6379,0)
+
+    # -----------------------------------------------------------------------
+    # 商户发布信息的方法
+    # -----------------------------------------------------------------------
 
     """
     商户发布团购信息
@@ -26,10 +31,10 @@ class ShopRedisDAO(object):
     param: groupon_ids 需要发布的团购id列表（一个list）
     """
     def publish_groupons(self,shop_id, groupon_ids):
-        serialNum = utils.SortedValueGenerator.generate_releaseDateValue()
         # 依次加入到zset里，并根据serialNum自动排序
         for groupon_id in groupon_ids:
-            redis.zadd(Constant.HOT_GROUPONS, groupon_id, serialNum)
+            serialNum = utils.SortedValueGenerator.generate_releaseDateValue() + shop_id * 10
+            self.redis.zadd(Constant.HOT_GROUPONS, groupon_id, serialNum)
 
     """
     商户发布折扣信息
@@ -44,3 +49,18 @@ class ShopRedisDAO(object):
             discount_value = utils.SortedValueGenerator.generate_discount_value()
             redis.zadd(Constant.DISCOUNTS_SORT, discount_id, discount_value)
 
+    # -----------------------------------------------------------------------
+    # 用户提取折扣、优惠券信息
+    # -----------------------------------------------------------------------
+
+    """
+    用户从移动终端提取目前热门的折扣信息
+    """
+    def get_available_discounts(self):
+        self.redis.smembers(Constant.DISCOUNTS_SORT)
+
+    """
+    用户从移动终端提取目前热门的团购信息
+    """
+    def get_available_groupons(self):
+        self.redis.smembers(Constant.HOT_GROUPONS)
